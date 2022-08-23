e0b84108dff3d9063f60a75a9f866ac2bcaecef1
unkn0wnb4ain <108135638+unkn0wnb4ain@users.noreply.github.com>
2022-08-24 02:38:11 +0530

    Create Negative_weight_weight_cycle.cpp

---
+++ pepcoding/Graphs/Negative_weight_weight_cycle.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+int32_t main()
+{
+	int n, m;
+	cin >> n >> m;		
+
+	vector <array <int, 3>> edges(m);
+
+	for(int i = 0; i < m; ++i)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+
+		edges[i] = {a, b, c};
+	}
+
+	vector <int> path(n + 1, INT_MAX);
+
+	path[1] = 0;
+
+	for(int i = 0; i < n - 1; i++)
+	{
+		for(int j = 0; j < m; ++j)
+		{
+			int a = edges[j][0];
+			int b = edges[j][1];
+			int c = edges[j][2];
+
+			if(path[a] == INT_MAX)
+				continue;
+
+			if(path[a] + c < path[b])
+				path[b] = path[a] + c;
+		}
+	}
+
+	bool flag = 0;
+
+	for(int j = 0; j < m; ++j)
+	{
+		int a = edges[j][0];
+		int b = edges[j][1];
+		int c = edges[j][2];
+
+		if(path[a] == INT_MAX)
+			continue;
+
+		if(path[a] + c < path[b])
+			flag = 1;
+	}
+
+	cout << (flag ? 1 : 0) << "\n";
+
+}																			
