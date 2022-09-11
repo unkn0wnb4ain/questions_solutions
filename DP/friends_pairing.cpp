8a838c91a49ddf43bbb7039bc5b12559a230d7bb
unkn0wnb4ain <108135638+unkn0wnb4ain@users.noreply.github.com>
2022-09-11 21:22:14 +0530

    Create friends_pairing.cpp

---
+++ DP/friends_pairing.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+int32_t main()
+{
+	int n;
+	cin >> n;
+
+	vector <int> dp(n + 1, 0);
+
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for(int i = 3; i <= n; ++i)	
+		dp[i] = dp[i - 1] + (i - 1) * dp[i - 2];
+
+	cout << dp[n] << "\n";
+}
